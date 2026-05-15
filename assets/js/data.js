26aa739f67f6697c20d45cea6c741c04dedaf435
nguyenhung0905 <94441194+NguyenHung952@users.noreply.github.com>
2026-05-15 23:12:08 +0700

    Create data.js

---
+++ assets/js/data.js
@@ -0,0 +1,16 @@
+export const responsePools = {
+  mood: [
+    ["Nghe mood nay tối ghê"],
+    ["Kiểu em đang cạn pin á ha"]
+  ],
+
+  eq: [
+    ["Ừa, đọc là biết em mệt rồi"],
+    ["Nãy giờ em gồng hơi nhiều"]
+  ],
+
+  soft: [
+    ["Thôi nay nghỉ sớm đi em"],
+    ["Ừ, có ngày nó vậy thiệt"]
+  ]
+};
